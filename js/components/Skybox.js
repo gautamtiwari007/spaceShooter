350c0a12607a2f55540502aa863f8621eba58654
gautamtiwari007 <gautamtiwari142@gmail.com>
2021-06-22 11:12:10 +0530

    Update Skybox.js

--- js/components/Skybox.js
+++ js/components/Skybox.js
@@ -1,19 +1,75 @@
 
 function Skybox(scene, height) {
 	
+	    var cubeMaterials = [];
+	    var firebaseConfig = {
+		apiKey: "AIzaSyB2up9-H2w7OgbTJjubL7OUAK9uWjDOkJI",
+		authDomain: "ar-vr-12de3.firebaseapp.com",
+		projectId: "ar-vr-12de3",
+		storageBucket: "ar-vr-12de3.appspot.com",
+		messagingSenderId: "233235157468",
+		appId: "1:233235157468:web:62a38b06f5b83cc61dbf19",
+		measurementId: "G-2S0NRB26DN",
+	    };
+	    // Initialize Firebase
+	    firebase.initializeApp(firebaseConfig);
+
+	    var storage = firebase.storage();
+	    var storageRef = storage.ref();
+	    storageRef.child('corona_ft.png').getDownloadURL()
+		.then((url) => {
+		    var obj = document.getElementById('hiroMarker1');
+		 cubeMaterials[0] = new THREE.MeshBasicMaterial({ map: textureLoader.load(`${url}`), side: THREE.DoubleSide }); 
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+	     storageRef.child('corona_bk.png').getDownloadURL()
+		.then((url) => {
+		    var obj = document.getElementById('hiroMarker1');
+		 cubeMaterials[1] = new THREE.MeshBasicMaterial({ map: textureLoader.load(`${url}`), side: THREE.DoubleSide }); 
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+	     storageRef.child('corona_up.png').getDownloadURL()
+		.then((url) => {
+		    var obj = document.getElementById('hiroMarker1');
+		 cubeMaterials[2] = new THREE.MeshBasicMaterial({ map: textureLoader.load(`${url}`), side: THREE.DoubleSide }); 
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+	     storageRef.child('corona_dn.png').getDownloadURL()
+		.then((url) => {
+		    var obj = document.getElementById('hiroMarker1');
+		 cubeMaterials[3] = new THREE.MeshBasicMaterial({ map: textureLoader.load(`${url}`), side: THREE.DoubleSide }); 
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+	      storageRef.child('corona_rt.png').getDownloadURL()
+		.then((url) => {
+		    var obj = document.getElementById('hiroMarker1');
+		 cubeMaterials[4] = new THREE.MeshBasicMaterial({ map: textureLoader.load(`${url}`), side: THREE.DoubleSide }); 
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+	      storageRef.child('corona_lf.png').getDownloadURL()
+		.then((url) => {
+		    var obj = document.getElementById('hiroMarker1');
+		 cubeMaterials[5] = new THREE.MeshBasicMaterial({ map: textureLoader.load(`${url}`), side: THREE.DoubleSide }); 
+		})
+		.catch((error) => {
+		    console.log(error);
+		});
+
+
 	const textureLoader = new THREE.TextureLoader()
-	
+
 	var geometry = new THREE.CubeGeometry(3000, 3000, 3000)
 
-	var cubeMaterials = 
-	[
-		new THREE.MeshBasicMaterial({ map: textureLoader.load("../../assets/textures/ulukai/corona_ft.png"), side: THREE.DoubleSide }),
-		new THREE.MeshBasicMaterial({ map: textureLoader.load("../../assets/textures/ulukai/corona_bk.png"), side: THREE.DoubleSide }),
-		new THREE.MeshBasicMaterial({ map: textureLoader.load("../../assets/textures/ulukai/corona_up.png"), side: THREE.DoubleSide }),
-		new THREE.MeshBasicMaterial({ map: textureLoader.load("../../assets/textures/ulukai/corona_dn.png"), side: THREE.DoubleSide }),
-		new THREE.MeshBasicMaterial({ map: textureLoader.load("../../assets/textures/ulukai/corona_rt.png"), side: THREE.DoubleSide }),
-		new THREE.MeshBasicMaterial({ map: textureLoader.load("../../assets/textures/ulukai/corona_lf.png"), side: THREE.DoubleSide }),
-	]
 	var material = new THREE.MeshFaceMaterial(cubeMaterials)
 
 	var cube = new THREE.Mesh(geometry, material)
