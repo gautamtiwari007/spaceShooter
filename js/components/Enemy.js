9d495da06fd59bfb7a6aab8bea6e3af4d0b611ba
gautamtiwari007 <gautamtiwari142@gmail.com>
2021-06-22 11:21:19 +0530

    Update Enemy.js

--- js/components/Enemy.js
+++ js/components/Enemy.js
@@ -1,12 +1,35 @@
 
 function Enemy(scene, x, z) {
 	
+	const URL = "";
+	    var firebaseConfig = {
+		apiKey: "AIzaSyB2up9-H2w7OgbTJjubL7OUAK9uWjDOkJI",
+		authDomain: "ar-vr-12de3.firebaseapp.com",
+		projectId: "ar-vr-12de3",
+		storageBucket: "ar-vr-12de3.appspot.com",
+		messagingSenderId: "233235157468",
+		appId: "1:233235157468:web:62a38b06f5b83cc61dbf19",
+		measurementId: "G-2S0NRB26DN",
+	    };
+	    // Initialize Firebase
+	    firebase.initializeApp(firebaseConfig);
+	
+	var storage = firebase.storage();
+    	var storageRef = storage.ref();
+   	storageRef.child('enemy.glb').getDownloadURL()
+        .then((url) => {
+           URL = url;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+	
 	var modelLoader = new THREE.GLTFLoader()
 	this.model;
 
 	modelLoader.load
 		( 
-			"../../assets/models/enemy/enemy.gltf", 
+			URL, 
 			(function(obj)
 			{
 				this.model = obj.scene;
