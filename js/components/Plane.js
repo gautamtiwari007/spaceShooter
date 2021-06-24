377b87f20bb10e98951170082084e747a30ff932
gautamtiwari007 <gautamtiwari142@gmail.com>
2021-06-24 11:30:41 +0530

    Update Plane.js

--- js/components/Plane.js
+++ js/components/Plane.js
@@ -47,8 +47,42 @@ function Plane(scene) {
 
 				}).bind(this)
 			)
-				this.update = function() {
+			this.update = function() {
 				this.model.position.z -= 0.4;
+			}
+			this.handleInput = function(keyMap, camera) {
+		
+				if (keyMap[87]) {
+					if (keyMap[16]) {
+						this.model.position.z -= 1.5;
+						camera.position.z -= 1.5;
+					}
+					else {
+						this.model.position.z -= 1;
+						camera.position.z -= 1;
+					}
+				}
+				if (keyMap[83] && this.model.position.z < -1) {
+					this.model.position.z += 1;
+					camera.position.z += 1;
+				}
+				if (keyMap[68] && this.model.position.x < 15) {
+					this.model.position.x += 0.2;			
+					camera.position.x += 0.2;
+				}
+				if (keyMap[65] && this.model.position.x > -15) {
+					this.model.position.x -= 0.2;
+					camera.position.x -= 0.2;
+				}
+			}
+			this.launchMissile = function() {
+
+				var x = this.model.position.x;
+				var z = this.model.position.z - this.planeBndBox.getSize().z;
+
+				const m = new Missile(scene, x, z);
+
+				return m;
 			}
 			})
 			.catch((error) => {
@@ -59,40 +93,40 @@ function Plane(scene) {
 // 		this.model.position.z -= 0.4;
 // 	}
 
-	this.handleInput = function(keyMap, camera) {
+// 	this.handleInput = function(keyMap, camera) {
 		
-		if (keyMap[87]) {
-			if (keyMap[16]) {
-				this.model.position.z -= 1.5;
-				camera.position.z -= 1.5;
-			}
-			else {
-				this.model.position.z -= 1;
-				camera.position.z -= 1;
-			}
-		}
-		if (keyMap[83] && this.model.position.z < -1) {
-			this.model.position.z += 1;
-			camera.position.z += 1;
-		}
-		if (keyMap[68] && this.model.position.x < 15) {
-			this.model.position.x += 0.2;			
-			camera.position.x += 0.2;
-		}
-		if (keyMap[65] && this.model.position.x > -15) {
-			this.model.position.x -= 0.2;
-			camera.position.x -= 0.2;
-		}
-	}
+// 		if (keyMap[87]) {
+// 			if (keyMap[16]) {
+// 				this.model.position.z -= 1.5;
+// 				camera.position.z -= 1.5;
+// 			}
+// 			else {
+// 				this.model.position.z -= 1;
+// 				camera.position.z -= 1;
+// 			}
+// 		}
+// 		if (keyMap[83] && this.model.position.z < -1) {
+// 			this.model.position.z += 1;
+// 			camera.position.z += 1;
+// 		}
+// 		if (keyMap[68] && this.model.position.x < 15) {
+// 			this.model.position.x += 0.2;			
+// 			camera.position.x += 0.2;
+// 		}
+// 		if (keyMap[65] && this.model.position.x > -15) {
+// 			this.model.position.x -= 0.2;
+// 			camera.position.x -= 0.2;
+// 		}
+// 	}
 
-	this.launchMissile = function() {
+// 	this.launchMissile = function() {
 
-        var x = this.model.position.x;
-        var z = this.model.position.z - this.planeBndBox.getSize().z;
+//         var x = this.model.position.x;
+//         var z = this.model.position.z - this.planeBndBox.getSize().z;
 
-        const m = new Missile(scene, x, z);
+//         const m = new Missile(scene, x, z);
 
-        return m;
-	}
+//         return m;
+// 	}
 }
 
