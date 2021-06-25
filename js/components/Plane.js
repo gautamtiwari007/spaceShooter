503800647d588b44f888db6a12d549451c4b522e
gautamtiwari007 <gautamtiwari142@gmail.com>
2021-06-25 11:44:49 +0530

    Update Plane.js

--- js/components/Plane.js
+++ js/components/Plane.js
@@ -45,47 +45,47 @@ function Plane(scene) {
  					this.planeBndBox = new THREE.Box3().setFromObject(this.model);
 
 				}).bind(this)
-			);
-			this.update = function() {
-				this.model.position.z -= 0.4;
-			}
-		
-			this.handleInput = function(keyMap, camera) {
-		
-				if (keyMap[87]) {
-					if (keyMap[16]) {
-						this.model.position.z -= 1.5;
-						camera.position.z -= 1.5;
-					}
-					else {
-						this.model.position.z -= 1;
-						camera.position.z -= 1;
-					}
-				}
-				if (keyMap[83] && this.model.position.z < -1) {
-					this.model.position.z += 1;
-					camera.position.z += 1;
-				}
-				if (keyMap[68] && this.model.position.x < 15) {
-					this.model.position.x += 0.2;			
-					camera.position.x += 0.2;
-				}
-				if (keyMap[65] && this.model.position.x > -15) {
-					this.model.position.x -= 0.2;
-					camera.position.x -= 0.2;
-				}
-			}
-			this.launchMissile = function() {
-
-				var x = this.model.position.x;
-				var z = this.model.position.z - this.planeBndBox.getSize().z;
-
-				const m = new Missile(scene, x, z);
-
-				return m;
-			}
+			)
 			})
 			.catch((error) => {
 			    console.log(error);
 			});
+	this.update = function() {
+				this.model.position.z -= 0.4;
+			}
+		
+	this.handleInput = function(keyMap, camera) {
+
+		if (keyMap[87]) {
+			if (keyMap[16]) {
+				this.model.position.z -= 1.5;
+				camera.position.z -= 1.5;
+			}
+			else {
+				this.model.position.z -= 1;
+				camera.position.z -= 1;
+			}
+		}
+		if (keyMap[83] && this.model.position.z < -1) {
+			this.model.position.z += 1;
+			camera.position.z += 1;
+		}
+		if (keyMap[68] && this.model.position.x < 15) {
+			this.model.position.x += 0.2;			
+			camera.position.x += 0.2;
+		}
+		if (keyMap[65] && this.model.position.x > -15) {
+			this.model.position.x -= 0.2;
+			camera.position.x -= 0.2;
+		}
+	}
+	this.launchMissile = function() {
+
+		var x = this.model.position.x;
+		var z = this.model.position.z - this.planeBndBox.getSize().z;
+
+		const m = new Missile(scene, x, z);
+
+		return m;
+	}
 }
