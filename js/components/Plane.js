0fd749bdc121f36001992b92855b64fa55442fd5
gautamtiwari007 <gautamtiwari142@gmail.com>
2021-06-25 11:12:58 +0530

    Update Plane.js

--- js/components/Plane.js
+++ js/components/Plane.js
@@ -1,13 +1,12 @@
 
 function Plane(scene) {
 	
-	var modelMaterial;
-	
 	const textureLoader = new THREE.TextureLoader()
-	
+	var texMap;
+	var modelMaterial;
 	storageRef.child('plane.png').getDownloadURL()
 		.then((url) => {
-		 var texMap = textureLoader.load(`${url}`);
+		 texMap = textureLoader.load(`${url}`);
 		 modelMaterial = new THREE.MeshBasicMaterial({ map: texMap});
 		})
 		.catch((error) => {
@@ -15,7 +14,6 @@ function Plane(scene) {
 		});
 	
 	var modelLoader = new THREE.OBJLoader()
-// 	var modelLoader = new THREE.GLTFLoader()
 
 	this.model;
  	this.planeBndBox;
@@ -47,10 +45,11 @@ function Plane(scene) {
  					this.planeBndBox = new THREE.Box3().setFromObject(this.model);
 
 				}).bind(this)
-			)
+			);
 			this.update = function() {
 				this.model.position.z -= 0.4;
 			}
+		
 			this.handleInput = function(keyMap, camera) {
 		
 				if (keyMap[87]) {
@@ -90,45 +89,3 @@ function Plane(scene) {
 			    console.log(error);
 			});
 }
-	
-// 	this.update = function() {
-// 		this.model.position.z -= 0.4;
-// 	}
-
-// 	this.handleInput = function(keyMap, camera) {
-		
-// 		if (keyMap[87]) {
-// 			if (keyMap[16]) {
-// 				this.model.position.z -= 1.5;
-// 				camera.position.z -= 1.5;
-// 			}
-// 			else {
-// 				this.model.position.z -= 1;
-// 				camera.position.z -= 1;
-// 			}
-// 		}
-// 		if (keyMap[83] && this.model.position.z < -1) {
-// 			this.model.position.z += 1;
-// 			camera.position.z += 1;
-// 		}
-// 		if (keyMap[68] && this.model.position.x < 15) {
-// 			this.model.position.x += 0.2;			
-// 			camera.position.x += 0.2;
-// 		}
-// 		if (keyMap[65] && this.model.position.x > -15) {
-// 			this.model.position.x -= 0.2;
-// 			camera.position.x -= 0.2;
-// 		}
-// 	}
-
-// 	this.launchMissile = function() {
-
-//         var x = this.model.position.x;
-//         var z = this.model.position.z - this.planeBndBox.getSize().z;
-
-//         const m = new Missile(scene, x, z);
-
-//         return m;
-// 	}
-
-
